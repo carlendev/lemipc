f77d9ac2efb1046fe0ccfee8b50a94aeba82e2f1
Yann Carlen <yann.carlen@epitech.eu>
2017-03-29 17:42:24 +0200

    map random generate

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 /**
  * Created by carlen on 3/14/17.
  */
-import { app, generateMap, size, mapObj } from '../src/app'
+import { app, generateMap, generateMapObj, size} from '../src/app'
 const request = require('supertest').agent(app.listen())
 
 describe('[GET] Map', () => {
@@ -9,15 +9,6 @@ describe('[GET] Map', () => {
         request
             .get('/api/map')
             .expect(200)
-            .expect({ map: generateMap(size).toString() }, done)
-    })
-})
-
-describe('[GET] Map Obj', () => {
-    it('should return the map obj', (done) => {
-        request
-            .get('/api/map/obj')
-            .expect(200)
-            .expect({ map: mapObj }, done)
+            .expect({ map: generateMapObj(size, generateMap(size)).toString() }, done)
     })
 })
