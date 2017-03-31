6fe16b08fa0d8ca17862bd536d5f7b2aede38638
Yann Carlen <yann.carlen@epitech.eu>
2017-03-31 20:38:01 +0200

    basic movement

--- client.js
+++ client.js
@@ -10,6 +10,7 @@ const getRandom = max => Math.floor(Math.random() * max) + 1
 let live = false
 const player = {}
 const db = createClient()
+let first = true
 
 const weshOut = () => {
     wesh('Usage ./client.js team id')
@@ -37,8 +38,46 @@ const main = argv => {
         wesh('pos')
         const players = JSON.parse(await db.get('players'))
         const size = parseInt(await db.get('size')) - 1
-        const x = getRandom(size)
-        const y = getRandom(size)
+        let x;
+        let y;
+        if (first) {
+            x = getRandom(size)
+            y = getRandom(size)
+            first = false
+        }
+        else {
+            const mine = players[`${player.team}${player.id}`]
+            x = mine.pos.x
+            y = mine.pos.y
+            if (x + 1 == size && y + 1 == size) --x
+            else if (x + 1 == size) {
+                --x
+                if (getRandom(2) == 1) ++y
+                else --y
+            }
+            else if (y + 1 == size) {
+                if (getRandom(2) == 1) ++x
+                else --x
+            }
+            else if (x - 1 == 0 && y - 1 == 0) ++x
+            else if (x - 1 == 0) {
+                ++x
+                if (getRandom(2) == 1) ++y
+                else --y
+            }
+            else if (y - 1 == 0) {
+                ++y
+                if (getRandom(2) == 1) --x
+                else ++x
+            }
+            else {
+                if (getRandom(2) == 1) --y
+                else ++y
+                if (getRandom(2) == 1) --x
+                else ++x
+            }
+        }
+        wesh(`${x} ${y}`)
         const pos = { x, y }
         players[`${player.team}${player.id}`].pos = pos
         wesh(players[`${player.team}${player.id}`])
