1e39ef99d4d80a348d140443d4e527410c4a91e8
Yann Carlen <yann.carlen@epitech.eu>
2017-03-31 17:10:49 +0200

    add client commande line interface

--- client.js
+++ client.js
@@ -1,23 +1,40 @@
 /**
  * Created by carlen on 3/24/17.
  */
-const socket = require('socket.io-client')('http://127.0.0.1:3030')
+const socket = require('socket.io-client')
 
 const wesh = msg => console.log(msg)
 
 let live = false
+const player = {}
 
-socket.on('connect',() => {
-    socket.emit('addTeam', { team: 'test', id: 1 })
-})
+const weshOut = () => {
+    wesh('Usage ./client.js team id')
+    process.exit(0)
+}
 
-socket.on('beginLive', (data) => {
-    live = true
-    wesh(`Begin to live ${data.team}${data.id}`)
-})
+const main = argv => {
+    if (argv.length != 4) weshOut()
+    const [ team, id ] = argv.filter(entity => !isNaN(entity)).filter(entity => entity > 0)
+    if (team === undefined || id === undefined) weshOut()
+    player.team = team
+    player.id = id
+    const io = socket.connect('http://127.0.0.1:3030')
 
-socket.on('disconnect', () => {
-    live = false
-    wesh(`I am out`)
-    //TODO(carlendev) exit process
-})
+    io.on('connect', () => {
+        io.emit('addTeam', { team: player.team, id: player.id })
+    })
+
+    io.on('beginLive', (data) => {
+        live = true
+        wesh(`Begin to live ${data.team}${data.id}`)
+    })
+
+    io.on('disconnect', () => {
+        live = false
+        wesh(`I am out`)
+        process.exit(0)
+    })
+}
+
+main(process.argv)
